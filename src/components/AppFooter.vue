d17661c6852f25cd709a22c735143724d3768328
diogenes0803 <soy0803@gmail.com>
2018-10-06 00:52:17 -0600

    Update AppFooter.vue

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mastfoot">
     <div class="inner">
-      <a href="https://github.com/devyhj" target="_blank" class="col-2"><icon name="github" scale="2"></icon></a>
+      <a href="https://github.com/diogenes0803" target="_blank" class="col-2"><icon name="github" scale="2"></icon></a>
       <a href="https://www.linkedin.com/in/hojuneyoo/" target="_blank" class="col-2"><icon name="linkedin-square" scale="2"></icon></a>
     </div>
   </div>
